7c3998672eb49ea597be16ed55fbfb23ceea9ea3
Solomon Akuson <akusonsolomon15@gmail.com>
2023-10-16 16:17:31 +0100

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element="Home page" />
+          <Route path="/" element={<SignUp />} />
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='w-screen h-screen flex flex-col items-center justify-center'>
+
+    </div>
+  )
+}
+
+export default SignUp
