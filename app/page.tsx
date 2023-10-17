eb73bc5700c7030b8444d8df0dfce186064b5cd4
Solomon Akuson <akusonsolomon15@gmail.com>
2023-10-17 11:22:49 +0100

    fixes

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='w-screen h-screen flex flex-col items-center justify-center bg-dark_green-900'>
+      <form action="" className='h-[80%] w-[35%] bg-white shadow-md rounded-xl'>
+
+      </form>
+    </div>
+  )
+}
