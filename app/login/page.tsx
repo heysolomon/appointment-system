a7ff2b53cc62a17d8c7c4fd83efb5919786e6e84
Solomon Akuson <akusonsolomon15@gmail.com>
2023-10-17 14:45:55 +0100

    fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import { BackgroundImage } from '@/assets/images'
 import Link from 'next/link'
 export default function Login() {
   return (
-    <div className='w-screen h-screen flex flex-col items-center justify-center bg-dark_green-900 fixed'>
+    <div className='w-screen h-screen flex flex-col items-center justify-center bg-tea_green-700 fixed'>
       <BackgroundImage />
       <form action="" className='w-[35%] bg-white shadow-md rounded-xl p-5 absolute z-50'>
         {/* title */}
@@ -15,31 +15,29 @@ export default function Login() {
           <p className='text-xs text-dark_green-600'>Please enter your details</p>
         </div>
 
+        <div className="mb-3 mt-5">
+            <label htmlFor="email" className="text-sm font-semibold">Email address</label>
+            <input type="email" id='email' name="email" className="w-full h-[35px] border border-dark_green-900 focus:ring-1 focus:ring-dark_green-700 focus:outline-none pl-1 text-sm rounded-md" />
+          </div>
+
+        <div className="">
+          <label htmlFor="password" className="text-sm font-semibold">Password</label>
+          <input type="password" id='password' name="password" className="w-full h-[35px] border border-dark_green-900 focus:ring-1 focus:ring-dark_green-700 focus:outline-none pl-1 text-sm rounded-md" />
+        </div>
+
+        <div className='flex justify-end mt-3 mb-5'>
+            <Link href='/forgot%20password' className='underline text-sm'>Forgot Password?</Link>
+        </div>
+
+        <button type='submit' className='w-full py-2 text-sm bg-tea_green-100 text-white rounded-md'>Sign Up</button>
+
         {/* google login */}
-        <div className='w-full mt-5'>
+        <div className='w-full mt-3'>
           <button type="button" className='flex items-center justify-center h-[35px]  border border-dark_green-900 text-xs rounded-md w-full'>
             <FcGoogle size={20} className="mr-2" />
             <p>Sign in with Google</p>
           </button>
         </div>
-
-        {/* or divition */}
-        <div className='w-full flex items-center mt-5 mb-3'>
-          <div className='h-[1px] w-full bg-dark_green-900' />
-          <p className='mx-3 text-xs text-dark_green-300'>OR</p>
-          <div className='h-[1px] w-full bg-dark_green-900' />
-        </div>
-        <div className="mb-3">
-            <label htmlFor="email" className="text-sm font-semibold">Email address</label>
-            <input type="email" id='email' name="email" className="w-full h-[35px] border border-dark_green-900 focus:ring-1 focus:ring-dark_green-700 focus:outline-none pl-1 text-sm rounded-md" />
-          </div>
-
-        <div className="mb-10">
-          <label htmlFor="password" className="text-sm font-semibold">Password</label>
-          <input type="password" id='password' name="password" className="w-full h-[35px] border border-dark_green-900 focus:ring-1 focus:ring-dark_green-700 focus:outline-none pl-1 text-sm rounded-md" />
-        </div>
-
-        <button type='submit' className='w-full py-2 text-sm bg-tea_green-100 text-white rounded-md'>Sign Up</button>
 
         <div className='w-full flex justify-center text-sm text-dark_green-300 my-3'>
           <p>Dont have an account? <Link href='/' className='underline font-semibold'>Sign Up</Link></p>
