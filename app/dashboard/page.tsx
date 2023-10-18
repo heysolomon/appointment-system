0c2888dfbc70dec9a917c3e48a673cf5ace11ad3
Solomon Akuson <akusonsolomon15@gmail.com>
2023-10-18 21:30:30 +0100

    added dashboard home

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,19 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className='p-10 h-full'>
+            <Tabs defaultValue="account" className="w-full">
+                <TabsList>
+                    <TabsTrigger value="upcoming" className="px-5 text-sm">Upcoming Appointments</TabsTrigger>
+                    <TabsTrigger value="canceled" className="px-5 text-sm">Canceled Appointments</TabsTrigger>
+                </TabsList>
+                <TabsContent value="upcoming" className=''>Upcoming appointments goes here</TabsContent>
+                <TabsContent value="canceled">canceled appointments goes here</TabsContent>
+            </Tabs>
+        </div>
+    )
 }
 
 export default page
