9a4d7cc155339c3bdde8b51aea1b7b61f8d94329
Solomon Akuson <akusonsolomon15@gmail.com>
2023-12-14 00:14:41 +0100

    fix

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,13 +1,13 @@
-import { ObjectId } from "mongoose";
-import NextAuth, { DefaultSession } from "next-auth"
+// import NextAuth, { DefaultSession } from "next-auth"
 
-declare module "next-auth" {
-    /**
-     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-    */
-    interface Session {
-        user: {
-            id: ObjectId;
-        } & DefaultSession["user"]
-    }
-}
+// declare module "next-auth" {
+//     /**
+//      * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+//     */
+//     interface Session {
+//         user: {
+//             id: string;
+//             email: string;
+//         } & DefaultSession["user"]
+//     }
+// }
