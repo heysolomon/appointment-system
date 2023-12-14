437cd2abd839eb416e060ecdbb5e81c1b3f8a79d
Solomon Akuson <akusonsolomon15@gmail.com>
2023-12-14 12:10:33 +0100

    created register route

---
+++ app/api/register/route.ts
@@ -0,0 +1,39 @@
+import User from "@/models/UserSchema";
+import { connectToDB } from "@/utils/db";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+export async function POST(req: Request) {
+    try {
+        await connectToDB()
+
+        const { email, name, password } = await req.json()
+
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return NextResponse.json({
+                message: "User already exists"
+            }, { status: 400 });
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = {
+            email,
+            name,
+            password: hashedPassword,
+        }
+
+        await User.create(user)
+
+        return NextResponse.json({
+            message: "User registered"
+        }, { status: 201 })
+    } catch (err) {
+        return NextResponse.json({
+            message: "Failed to register user"
+        }, { status: 500 })
+    }
+}
