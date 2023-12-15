a51fe776a237686795bbeb68fa41744d1dd0ec0d
Solomon Akuson <akusonsolomon15@gmail.com>
2023-12-15 11:24:37 +0100

    improved signup form

--- components/auth/SignUp.tsx
+++ components/auth/SignUp.tsx
@@ -1,16 +1,77 @@
 'use client'
 
+import { Schema, z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
 import { NsukLogo } from "@/public/assets/icons/icons"
 import { AuthImage } from "@/public/assets/images/images"
 import { ClientSafeProvider, LiteralUnion, getProviders, signIn } from "next-auth/react"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 import { FcGoogle } from "react-icons/fc"
+import { Label } from "../ui/label"
+import { Input } from "../ui/input";
+
 
 const SignUp = () => {
     type BuiltInProviderType = 'google' | 'facebook' | 'twitter';
 
     const [providers, setProviders] = useState<Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null>(null)
+
+    const { toast } = useToast()
+
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const [message, setMessage] = useState('');
+
+    const checkStart = () => {
+        setError(false);
+        setSuccess(false);
+        setLoading(true);
+    };
+
+    const checkSuccess = (msg: string) => {
+        setSuccess(true);
+        setLoading(false);
+        setMessage(msg);
+    };
+
+    const checkFailed = (msg: string) => {
+        setLoading(false);
+        setError(true);
+        setMessage(msg);
+    };
+
+    const registerSchema: Schema = z.object({
+        email: z.string().email('email is invalid'),
+        name: z.string().min(3, 'must contain 3 or more characters'),
+        password: z.string({
+            required_error: 'password is required',
+        }).regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+            `Must Contain One Uppercase, One Lowercase,
+        One Number and one special case Character`).min(8, 'password must be atleast 8 characters long'),
+        confirmPassword: z.string().refine(data => data === registerSchema.password, {
+            message: 'Passwords must match',
+        })
+    });
+
+    const form = useForm<z.infer<typeof registerSchema>>({
+        resolver: zodResolver(registerSchema),
+        defaultValues: {
+            email: "",
+            name: "",
+            password: "",
+            confirmPassword: "",
+        },
+    })
+
+    const onSubmit = (values: z.infer<typeof registerSchema>) => {
+        console.log(values)
+    }
 
     useEffect(() => {
         (async () => {
@@ -26,66 +87,92 @@ const SignUp = () => {
                     <AuthImage className='w-full' />
                 </div>
                 <div className='w-full h-full flex items-center justify-center md:pr-5'>
-                    <form action="" className='w-full bg-white shadow-md rounded-xl p-5'>
-                        {/* title */}
-                        <div className='flex items-center gap-5'>
-                            <NsukLogo />
-                            <div>
-                                <h1 className="text-dark_green-500 font-semibold text-md md:text-lg">Welcome to the VC appointment scheduling system</h1>
-                                <p className='text-xs text-dark_green-600'>Please provide your details to sign up</p>
-                            </div>
-                        </div>
-
-                        <div className='px-3'>
-                            {/* google login */}
-                            <div className='w-full mt-10'>
-                                {providers && Object.values(providers).map(provider => (
-                                    <button
-                                        type="button"
-                                        key={provider.id}
-                                        onClick={() => {
-                                            signIn(provider.id, { callbackUrl: process.env.NODE_ENV === "production" ? "https://appointment-system-ten.vercel.app/dashboard" : "http://localhost:3000/dashboard" });
-                                        }}
-                                        className='flex items-center justify-center h-[35px]  border border-dark_green-900 text-xs rounded-md w-full'>
-                                        <FcGoogle size={20} className="mr-2" />
-                                        <p>Sign up with Google</p>
-                                    </button>
-                                ))}
-                            </div>
-
-                            {/* or divition */}
-                            <div className='w-full flex items-center mt-5 mb-10'>
-                                <div className='h-[1px] w-full bg-dark_green-900' />
-                                <p className='mx-3 text-xs text-dark_green-300'>OR</p>
-                                <div className='h-[1px] w-full bg-dark_green-900' />
-                            </div>
-                            <div className='grid grid-cols-1 md:grid-cols-2 gap-3 mb-3'>
-                                <div className="">
-                                    <label htmlFor="email" className="text-sm font-semibold">Email address</label>
-                                    <input type="email" id='email' name="email" className="w-full h-[35px] border border-dark_green-900 focus:ring-1 focus:ring-dark_green-700 focus:outline-none pl-1 text-sm rounded-md" />
-                                </div>
-                                <div className="">
-                                    <label htmlFor="name" className="text-sm font-semibold">Fullname</label>
-                                    <input type="text" id='name' name="name" className="w-full h-[35px] border border-dark_green-900 focus:ring-1 focus:ring-dark_green-700 focus:outline-none pl-1 text-sm rounded-md" />
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full bg-white shadow-md rounded-xl p-5">
+                            {/* title */}
+                            <div className='flex items-center gap-5'>
+                                <NsukLogo />
+                                <div>
+                                    <h1 className="text-dark_green-500 font-semibold text-md md:text-lg">Welcome to the VC appointment scheduling system</h1>
+                                    <p className='text-xs text-dark_green-600'>Please provide your details to sign up</p>
                                 </div>
                             </div>
 
-                            <div className="mb-3">
-                                <label htmlFor="password" className="text-sm font-semibold">Password</label>
-                                <input type="password" id='password' name="password" className="w-full h-[35px] border border-dark_green-900 focus:ring-1 focus:ring-dark_green-700 focus:outline-none pl-1 text-sm rounded-md" />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="confirmPassword" className="text-sm font-semibold">Confirm Password</label>
-                                <input type="password" id='confirmPassword' name="confirmPassword" className="w-full h-[35px] border border-dark_green-900 focus:ring-1 focus:ring-dark_green-700 focus:outline-none pl-1 text-sm rounded-md" />
-                            </div>
+                            <div className='px-3'>
+                                {/* google login */}
+                                <div className='w-full mt-10'>
+                                    {providers && Object.values(providers).map(provider => (
+                                        <button
+                                            type="button"
+                                            key={provider.id}
+                                            onClick={() => {
+                                                signIn(provider.id, { callbackUrl: process.env.NODE_ENV === "production" ? "https://appointment-system-ten.vercel.app/dashboard" : "http://localhost:3000/dashboard" });
+                                            }}
+                                            className='flex items-center justify-center h-[35px]  border border-dark_green-900 text-xs rounded-md w-full'>
+                                            <FcGoogle size={20} className="mr-2" />
+                                            <p>Sign up with Google</p>
+                                        </button>
+                                    ))}
+                                </div>
 
-                            <button type='submit' className='w-full py-2 text-sm bg-tea_green-100 text-white rounded-md mt-5'>Sign Up</button>
+                                {/* or divition */}
+                                <div className='w-full flex items-center mt-5 mb-10'>
+                                    <div className='h-[1px] w-full bg-dark_green-900' />
+                                    <p className='mx-3 text-xs text-dark_green-300'>OR</p>
+                                    <div className='h-[1px] w-full bg-dark_green-900' />
+                                </div>
 
-                            <div className='w-full flex justify-center text-sm text-dark_green-300 my-3'>
-                                <p>Already have an account? <Link href='/login' className='underline font-semibold'>Sign in</Link></p>
+                                <div className='grid grid-cols-1 md:grid-cols-2 gap-3 mb-3'>
+                                    <FormField control={form.control} name="email" render={({ field }) => (
+                                        <FormItem className="mt-3">
+                                            <Label htmlFor="email" className="text-sm font-semibold">Email address</Label>
+                                            <FormControl>
+                                                <Input type="email" id='email' name="email" className="w-full h-[35px] pl-1 text-sm rounded-md" />
+                                            </FormControl>
+                                            <FormMessage className="text-red-500 text-xs" />
+                                        </FormItem>
+                                    )} />
+
+                                    <FormField control={form.control} name="name" render={({ field }) => (
+                                        <FormItem className="mt-3">
+                                            <Label htmlFor="name" className="text-sm font-semibold">Fullname</Label>
+                                            <FormControl>
+                                                <Input type="text" id='name' name="name" className="w-full h-[35px] pl-1 text-sm rounded-md" />
+                                            </FormControl>
+                                            <FormMessage className="text-red-500 text-xs" />
+                                        </FormItem>
+                                    )} />
+
+                                </div>
+
+                                <FormField control={form.control} name="password" render={({ field }) => (
+                                    <FormItem className="mb-3">
+                                        <Label htmlFor="password" className="text-sm font-semibold">Password</Label>
+                                        <FormControl>
+                                            <Input type="password" id='password' name="password" className="w-full h-[35px] pl-1 text-sm rounded-md" />
+                                        </FormControl>
+                                        <FormMessage className="text-red-500 text-xs" />
+                                    </FormItem>
+                                )} />
+
+                                <FormField control={form.control} name="confirmPassword" render={({ field }) => (
+                                    <FormItem className="mb-3">
+                                        <Label htmlFor="confirmPassword" className="text-sm font-semibold">Confirm Password</Label>
+                                        <FormControl>
+                                            <Input type="password" id='confirmPassword' name="confirmPassword" className="w-full h-[35px] pl-1 text-sm rounded-md" />
+                                        </FormControl>
+                                        <FormMessage className="text-red-500 text-xs" />
+                                    </FormItem>
+                                )} />
+
+                                <button type='submit' className='w-full py-2 text-sm bg-tea_green-100 text-white rounded-md mt-5'>Sign Up</button>
+
+                                <div className='w-full flex justify-center text-sm text-dark_green-300 my-3'>
+                                    <p>Already have an account? <Link href='/login' className='underline font-semibold'>Sign in</Link></p>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </Form>
                 </div>
             </div>
         </div>
