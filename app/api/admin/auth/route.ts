25c6cc497da484e744871e55a0d9e86a3896c5f0
Solomon Akuson <akusonsolomon15@gmail.com>
2024-01-11 21:13:12 +0100

    admin login auth api

---
+++ app/api/admin/auth/route.ts
@@ -0,0 +1,47 @@
+import User from "@/models/UserSchema";
+import { connectToDB } from "@/utils/db";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+export async function POST(req: Request) {
+    try {
+        await connectToDB()
+
+        const { email, password } = await req.json()
+
+        const userExists = await User.findOne({ email });
+
+        if (!userExists) {
+            return NextResponse.json({
+                message: "Invalid credentials"
+            }, { status: 400 });
+        }
+
+        if (userExists.role === "user") {
+            return NextResponse.json({
+                message: "Invalid credentials"
+            }, { status: 400 });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, userExists.password);
+
+        if (!passwordMatch) {
+            return NextResponse.json({
+                message: "Invalid credentials"
+            }, { status: 400 });
+        }
+
+        return NextResponse.json({
+            message: "User registered",
+            user: {
+                email: userExists.email,
+                name: userExists.name,
+            }
+        }, { status: 200 })
+    } catch (err) {
+        NextResponse.json({
+            message: "Invalid credentials"
+        }, { status: 500 })
+        throw new Error("Invalid credentials")
+    }
+}
